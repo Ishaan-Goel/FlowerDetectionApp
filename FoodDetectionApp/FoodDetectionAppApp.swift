a780af6aca7c31761f867351cfa0c774e732f193
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 12:53:05 -0800

    Initial Commit

---
+++ FoodDetectionApp/FoodDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDetectionAppApp.swift
+//  FoodDetectionApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
